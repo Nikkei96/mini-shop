9c95be7173314c54f8d6144fc013c43a59a36ce3
Nikita <siberiaforce@yandex.ru>
2021-01-10 19:39:21 +0300

    2. Добавили router-link и установили фреймворк vuetify

--- src/view/Page1.vue
+++ src/view/Page1.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h2>Страница 1</h2>
+    <router-link to="/">Home</router-link>
+    <br>
+    <router-link to="/page2">Страница 2</router-link>
   </div>
 </template>
--- src/view/Page2.vue
+++ src/view/Page2.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h2>Страница 2</h2>
+    <router-link to="/">Home</router-link>
+    <br>
+    <router-link to="/page1">Страница 1</router-link>
   </div>
 </template>

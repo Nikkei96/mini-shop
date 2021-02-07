3f54b5f9ab4cb859724081b475f34aad386b283d
Nikita <siberiaforce@yandex.ru>
2021-02-07 21:35:40 +0300

    21. Реализовали компонент BalanceDialog.vue (шаблон), настроили его появление и исчезновение по нажатию на кнопку

---
+++ src/components/BalanceDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-dialog :value='dialog' @click:outside='$emit("changeDialog")' width='500'>
+    <v-card>
+      <v-card-title class='headline grey lighten-2 mb-4'>
+        Мой кошелёк
+        <v-spacer></v-spacer>
+        <v-btn color='primary' icon @click='$emit("changeDialog")'>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text>ЗДЕСЬ БУДЕТ КОНТЕНТ (ИНПУТЫ И Т.Д.)</v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['dialog'],
+}
+</script>
+
+<style>
+</style>
--- src/view/Balance.vue
+++ src/view/Balance.vue
@@ -1,10 +1,13 @@
 <template>
   <v-container fluid>
+    <BalanceDialog :dialog='dialog' @changeDialog='dialog = !dialog' />
     <Loader v-if='!currencies' />
     <v-card v-else>
       <v-card-title>
         <v-icon class='mr-2'>mdi-cash-multiple</v-icon>
         <span>Курс валют на {{new Date(currencies.Date) | dateTimeFilter}}</span>
+        <v-spacer></v-spacer>
+        <v-btn color='primary' @click='dialog = !dialog'>Кошелёк</v-btn>
       </v-card-title>
 
       <v-card-text>
@@ -28,14 +31,23 @@
 </template>
 
 <script>
+import BalanceDialog from '@/components/BalanceDialog'
+
 export default {
+  components: {
+    BalanceDialog,
+  },
+
   data: () => ({
     currencies: null,
+    dialog: false,
   }),
 
-  async mounted() {
-    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-    this.currencies = await response.json()
+  mounted() {
+    setTimeout(async () => {
+      const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+      this.currencies = await response.json()
+    }, 500)
   },
 }
 </script>

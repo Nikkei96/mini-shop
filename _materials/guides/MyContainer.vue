12ef08779acf2a1b969684b77af1d0dd9e9ac5bf
Nikita <siberiaforce@yandex.ru>
2021-02-13 23:20:58 +0300

    24. Реализовали свой ui-компонент MyContainer, получили более глубокое понимание как работает ui-фреймворк Vuetify

---
+++ _materials/guides/MyContainer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class='container'
+    :class='{"fluid" : fluid}'
+    :style='{"background-color": color}'
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fluid: {
+      type: Boolean,
+      default: false,
+    },
+
+    color: {
+      type: String,
+      default: '#999',
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  max-width: 900px;
+  padding: 0 20px;
+  background-color: #999;
+  margin: 0 auto;
+}
+
+.fluid {
+  max-width: unset;
+}
+</style>

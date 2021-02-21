5bce4e239f237ec1927910be42d10e7d2d1fd584
Nikita <siberiaforce@yandex.ru>
2021-02-21 14:45:23 +0300

    27. Сделали вывод инициалов и подготовили вывод аватарки, сделали hover для появления кнопки добавления авы

--- src/components/MainLayoutSideBarSheet.vue
+++ src/components/MainLayoutSideBarSheet.vue
@@ -3,12 +3,25 @@
     color='grey lighten-4'
     :class='window.width > windowLimit ? "pa-4" : "text-center"'
   >
-    <v-avatar
-      :class='window.width > windowLimit ? "mb-4" : "my-2"'
-      color='grey darken-1'
-      size='32'
-      :title='window.width < windowLimit ? name : ""'
-    ></v-avatar>
+    <v-hover v-slot='{hover}'>
+      <v-avatar
+        :class='window.width > windowLimit ? "mb-4" : "my-2"'
+        color='grey darken-1'
+        size='48'
+        :title='window.width < windowLimit ? name : ""'
+      >
+        <v-btn v-if='hover' fab dark color='#2196F3'>
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+        <template v-else>
+          <img v-if='userAvatar' :src='userAvatar' />
+          <span
+            v-else
+            class='white--text headline'
+          >{{ name ? name[0].toUpperCase() : null}}</span>
+        </template>
+      </v-avatar>
+    </v-hover>
     <div v-if='window.width > windowLimit'>{{name}}</div>
   </v-sheet>
 </template>
@@ -19,12 +32,14 @@ export default {
 
   data: () => ({
     name: null,
+    userAvatar: null,
   }),
 
   async created() {
     const uid = this.$firebase.auth().currentUser?.uid
     const dbData = (await this.$firebase.database().ref(`/users/${uid}/info`).once('value')).val()
     this.name = dbData.name
+    this.userAvatar = dbData.avatarUrl
   },
 }
 </script>

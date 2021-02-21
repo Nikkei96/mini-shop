7366b54190af54110551b29238a9713fb21e1019
Nikita <siberiaforce@yandex.ru>
2021-02-21 14:25:16 +0300

    26. Вывели баланс пользователя в хэдере (столкнулись с проблемой обновления хэдера, немного обсудили vuex)

--- src/components/MainLayoutHeader.vue
+++ src/components/MainLayoutHeader.vue
@@ -3,6 +3,8 @@
     <v-icon color='blue' class='mr-2'>mdi-shopping</v-icon>
     <v-toolbar-title>{{shopName}}</v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-icon class='mr-2' color='green'>mdi-cash-multiple</v-icon>
+    <div class='mr-6'>{{balance}}₽</div>
     <v-btn icon @click='logout'>
       <v-icon>mdi-exit-to-app</v-icon>
     </v-btn>
@@ -13,6 +15,7 @@
 export default {
   data: () => ({
     shopName: null,
+    balance: null,
   }),
 
   methods: {
@@ -30,6 +33,8 @@ export default {
   async mounted() {
     try {
       this.shopName = (await this.$firebase.database().ref('/shop_info/name').once('value')).val()
+      const uid = this.$firebase.auth().currentUser.uid
+      this.balance = (await this.$firebase.database().ref(`/users/${uid}/info`).once('value')).val().money
     } catch (e) {
       throw e
     }
